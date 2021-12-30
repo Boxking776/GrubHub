e14256e118e02e0c7edcc7ecbd299e46a1c4ca7c
Boxking776 <64418882+Boxking776@users.noreply.github.com>
2021-12-30 14:13:05 +0000

    Update ChangelogModule.lua

--- free/data/ChangelogModule.lua
+++ free/data/ChangelogModule.lua
@@ -222,6 +222,7 @@ Close.MouseButton1Down:Connect(function()
     openingMusicitf86te.PlayOnRemove = true
     openingMusicitf86te.Playing = false
     openingMusicitf86te:Destroy()
+    wait(0.5)
     Frame:TweenPosition(UDim2.new(1.5, 0,0, 1.5))
     wait(2)
     UpdateLog:Destroy()
