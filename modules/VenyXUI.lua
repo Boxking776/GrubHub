af0997edaac3fc7acd435b1cd0387e3282e7f8f5
botdevXD <54240316+botdevXD@users.noreply.github.com>
2022-02-11 02:33:25 +0000

    Update VenyXUI.lua

--- modules/VenyXUI.lua
+++ modules/VenyXUI.lua
@@ -1812,6 +1812,9 @@ do
 		
 		local updatedropdownlist = function(newlist)
 		    list = type(newlist) == "table" and newlist or {}
+		    if search.Button.Rotation == 0 then
+			self:updateDropdown(dropdown, nil, list, callback)
+		    end
 		end
 		
 		return dropdown, updatedropdownlist
